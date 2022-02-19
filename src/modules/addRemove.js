82bdc82a439dab28914ce70d813ee50bbcfb54a9
ForHemer <foreste.hemerson@gmail.com>
2022-02-19 16:16:55 -0500

    Correct addRemove.js file to take into account suggestion by reviewer

--- src/modules/addRemove.js
+++ src/modules/addRemove.js
@@ -1,5 +1,33 @@
-/* eslint-disable max-classes-per-file */
 import Task from './task.js';
+
+class displayTask {
+  static displayTasks() {
+    const tasks = Store.getTasks();
+    tasks.forEach((task) => displayTask.addTaskToList(task));
+  }
+
+  static addTaskToList(task) {
+    const elementList = document.querySelector('.list');
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'item');
+
+    listItem.innerHTML = `<input type="checkbox" id='${task.id}' name="taskStatus" class = 'taskStatus'>
+                      <p class='description edit' contenteditable='true' id='${task.id}'>${task.description}</p>
+                      <i class="fas fa-trash-alt remove-btn" id='${task.id}'></i>`;
+
+    elementList.appendChild(listItem);
+  }
+
+  static deleteTask(el) {
+    if (el.classList.contains('remove-btn')) {
+      el.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#inputTask').value = '';
+  }
+}
 
 class Store {
   static getTasks() {
@@ -25,12 +53,11 @@ class Store {
       if (tasks.length === 1) {
         tasks.pop();
       } else {
-        tasks.splice(parseInt(elem.id, 2), 1);
-
-        tasks.forEach((element, index) => {
-          element.id = index + 1;
-        });
+        tasks.splice(parseInt(elem.id), 1);
       }
+      tasks.forEach((element, index) => {
+        element.id = index + 1;
+      });
       index = tasks.length;
 
       localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -39,7 +66,7 @@ class Store {
 
   static modifyTask(elem) {
     const tasks = Store.getTasks();
-    tasks[elem.id].description = elem.innerHTML;
+    tasks[elem.id - 1].description = elem.innerHTML;
 
     tasks.forEach((element, index) => {
       element.id = index + 1;
@@ -50,7 +77,7 @@ class Store {
 
   static CompletedTask(elem) {
     const tasks = Store.getTasks();
-    tasks[elem.id].completed = true;
+    tasks[elem.id - 1].completed = true;
     tasks.forEach((element, index) => {
       element.id = index + 1;
     });
@@ -59,36 +86,8 @@ class Store {
   }
 }
 
-class displayTask {
-  static displayTasks() {
-    const tasks = Store.getTasks();
-    tasks.forEach((task) => displayTask.addTaskToList(task));
-  }
-
-  static addTaskToList(task) {
-    const elementList = document.querySelector('.list');
-    const listItem = document.createElement('li');
-    listItem.setAttribute('class', 'item');
-
-    listItem.innerHTML = `<input type="checkbox" id='${task.id}' name="taskStatus" class = 'taskStatus'>
-                    <p class='description edit' contenteditable='true' id='${task.id}'>${task.description}</p>
-                    <i class="fas fa-trash-alt remove-btn" id='${task.id}'></i>`;
-
-    elementList.appendChild(listItem);
-  }
-
-  static deleteTask(el) {
-    if (el.classList.contains('remove-btn')) {
-      el.parentElement.remove();
-    }
-  }
-
-  static clearFields() {
-    document.querySelector('#inputTask').value = '';
-  }
-}
-
 document.addEventListener('DOMContentLoaded', displayTask.displayTasks);
+
 let index = 1;
 
 // Event: Add a Task
@@ -111,7 +110,7 @@ document.querySelector('#inputTask').addEventListener('keyup', (event) => {
     if (description === '') {
       alert('Please fill in the field');
     } else {
-    // Instantiate task
+      // Instantiate task
       const task = new Task(description, id, completed);
 
       // Add Task to displayTask
@@ -148,7 +147,6 @@ document.querySelector('.list').addEventListener('change', (e) => {
 
   if (e.target.classList.contains('taskStatus')) {
     if (e.target.checked) {
-      // do something
       e.target.nextElementSibling.classList.add('line');
       Store.CompletedTask(e.target);
     }
@@ -157,11 +155,9 @@ document.querySelector('.list').addEventListener('change', (e) => {
 
 document.querySelector('.clrCompleted').addEventListener('click', (e) => {
   e.preventDefault();
-  let tasks = Store.getTasks();
-  const arr1 = tasks.filter((el) => el.completed === false);
-  tasks = arr1;
-  localStorage.setItem('tasks', JSON.stringify(tasks));
 
+  // taskCompleted interactive();
+  taskCompleted();
   const parent = document.querySelector('.list');
   while (parent.firstChild) {
     parent.firstChild.remove();
