4a27ae4f9b073b3001faee7b72287620b759ece1
ForHemer <foreste.hemerson@gmail.com>
2022-02-24 23:41:01 -0500

    Create taskList.test.js file

---
+++ src/tests/taskList.test.js
@@ -0,0 +1,33 @@
+import TaskList from '../modules/taskList.js';
+
+const list = new TaskList();
+const item1 = {
+  index: 1,
+  desc: 'test description',
+  completed: false,
+};
+const item2 = {
+  index: 2,
+  desc: 'test description 2',
+  completed: false,
+};
+
+describe('addNew method', () => {
+  test('updates the length to 1 of storage in TaskList', () => {
+    list.addNew(item1);
+    expect(list.storage).toHaveLength(1);
+  });
+
+  test('adds right item to the storage in TaskList', () => {
+    expect(list.storage[0]).toBe(item1);
+  });
+
+  test('updates the length to 2 of storage in TaskList', () => {
+    list.addNew(item2);
+    expect(list.storage).toHaveLength(2);
+  });
+
+  test('adds right item to the storage in TaskList', () => {
+    expect(list.storage[1]).toBe(item2);
+  });
+});
